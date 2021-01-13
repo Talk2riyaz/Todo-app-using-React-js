02713a6be0f47b56f1b9e2803e3c8a80e3a6c226
Riyaz Qureshi <65034428+Talk2riyaz@users.noreply.github.com>
2021-01-13 13:50:08 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Todo List</span> <br />
+      
+    </div>
+  );
+}
+
+export default App;
